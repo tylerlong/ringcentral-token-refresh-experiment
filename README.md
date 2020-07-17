b5f4d74daf30986c53c0ba4016bf0cfae582d96c
Tyler Long <tyler4long@gmail.com>
2020-07-16 20:09:06 -0700

    Conclusion

--- README.md
+++ README.md
@@ -3,17 +3,29 @@
 RingCentral Token Refresh experiment
 
 
-## conclusion
+## access token
 
-After token refresh, old access token becomes invalid immediately while refresh token remains valid.
+After token refresh, old access token **becomes invalid immediately**.
 
-### access token
+If you try to use the old access token right after the refreshing, error message is **"Access token corrupted"**. 
 
-If you try to use the old access token right after the refreshing, error message is "Access token corrupted". 
-
-About 10 seconds later, using the old access token generates the error message "Token not found"
+About 10 seconds, using the old access token generates error message **"Token not found"**.
 
 
-### Refresh token
+## Refresh token
 
-Refresh token remains valid after token refreshing.
+After token refresh, old refresh token remains valid for a while.
+
+If you **use the new access token**, old refresh token will become invalid in **10 seconds**. 
+Within 10 seconds, you can refresh **as many times as you want**.
+But every time you refresh you will get the **same access token**. 
+After 10 seconds, refresh it will generate error message **"Token not found"**.
+
+If you **don't use the new access token**, old refresh token will remain valid for **at least 30 minutes**.
+You can refresh **as many times as you want**.
+But every time you refresh you will get the **same access token**. 
+It **doesn't make much sense** to refresh it again and again because every time you get the same access token and if you use the access token the refresh token will become invalid in 10 seconds.
+
+Even an old refresh token remains valid for a while, every time you refresh you will get the **same access token**. 
+Which means, the server side simply gives you **cached value of the first refresh**.
+This is **by design**, in case that you did the fresh but failed to receive the result due to network issues.
--- test/refresh_token.spec.ts
+++ test/refresh_token.spec.ts
@@ -5,7 +5,7 @@ import waitFor from 'wait-for-async';
 jest.setTimeout(256000);
 
 describe('refresh token', () => {
-  test('still valid if immediately use it', async () => {
+  test('refresh and use access token', async () => {
     const rc = new RingCentral({
       clientId: process.env.RINGCENTRAL_CLIENT_ID!,
       clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
@@ -18,17 +18,41 @@ describe('refresh token', () => {
       password: process.env.RINGCENTRAL_PASSWORD!,
     });
 
-    const oldTokenFromDB = JSON.parse(JSON.stringify(rc.token));
-    await rc.refresh();
-    rc.token = oldTokenFromDB;
+    const oldToken = JSON.parse(JSON.stringify(rc.token));
 
-    // no exception, still works
+    rc.token = oldToken;
     await rc.refresh();
+    await rc.restapi().account().extension().get();
+    console.log(1);
+    await waitFor({interval: 3300});
+
+    rc.token = oldToken;
+    await rc.refresh();
+    await rc.restapi().account().extension().get();
+    console.log(2);
+    await waitFor({interval: 3300});
+
+    rc.token = oldToken;
+    await rc.refresh();
+    await rc.restapi().account().extension().get();
+    console.log(3);
+    await waitFor({interval: 3300});
+
+    // refresh token becomes invalid after 10 seconds
+    rc.token = oldToken;
+    let exception = false;
+    try {
+      await rc.refresh();
+    } catch (e) {
+      exception = true;
+      expect(e.message.includes('Token not found')).toBeTruthy();
+    }
+    expect(exception).toBeTruthy();
 
     await rc.revoke();
   });
 
-  test('use it after 240 seconds', async () => {
+  test("refresh but don't use access token", async () => {
     const rc = new RingCentral({
       clientId: process.env.RINGCENTRAL_CLIENT_ID!,
       clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
@@ -41,17 +65,36 @@ describe('refresh token', () => {
       password: process.env.RINGCENTRAL_PASSWORD!,
     });
 
-    const oldTokenFromDB = JSON.parse(JSON.stringify(rc.token));
+    const oldToken = JSON.parse(JSON.stringify(rc.token));
+
+    rc.token = oldToken;
     await rc.refresh();
-    rc.token = oldTokenFromDB;
+    console.log(1);
+    await waitFor({interval: 60000});
 
-    await waitFor({interval: 240000});
-
-    // no exception, still works
+    rc.token = oldToken;
     await rc.refresh();
+    console.log(2);
+    await waitFor({interval: 60000});
 
-    // no exception
-    await rc.restapi().account().extension().get();
+    rc.token = oldToken;
+    await rc.refresh();
+    console.log(3);
+    await waitFor({interval: 60000});
+
+    rc.token = oldToken;
+    await rc.refresh();
+    console.log(4);
+    await waitFor({interval: 60000});
+
+    rc.token = oldToken;
+    await rc.refresh();
+    console.log(5);
+    await waitFor({interval: 60000});
+
+    rc.token = oldToken;
+    await rc.refresh();
+    console.log(6);
 
     await rc.revoke();
   });
